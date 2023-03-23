64f10aa928aae92601e39e6bf0f55dea6f070f1c
mateaivanoska <mateaivanoska@gmail.com>
2023-03-23 14:09:36 +0100

    first commit

---
+++ src/main/java/mk/ukim/finki/emt_l/EmtLApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.emt_l;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmtLApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmtLApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mk/ukim/finki/emt_l/EmtLApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.emt_l;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmtLApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
